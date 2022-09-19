3d25df447558eed02f1da0f504f27b4a36d78277
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-09-19 21:17:56 +0500

    Git Init

--- Express/index.js
+++ Express/index.js
@@ -5,12 +5,24 @@ const app = express();
 
 const port = 4000;
 
-app.get('/', (req, res) => {
+// const middleWare = (req, res, next) => {
+//     console.log(req);
+// }
+
+app.use(express.static(path.join(__dirname, "public")));
+// app.use(middleWare);
+
+app.get('/home', (req, res) => {
     res.send('<h1>Learning Express.JS!</h1>')
 })
 
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, "/about.html"));
+    res.status(202);
+})
+
+app.get('/data', (req, res) => {
+    res.json({ "ahmed": 4 });
 })
 
 app.listen(port, () => {
