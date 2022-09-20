926745764754c33bb2fcecafdeed0f6af576f011
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2022-09-20 14:10:28 +0500

    Git Init

---
+++ Express/BlogExpress/data/blogs.js
@@ -0,0 +1,29 @@
+blogs = [
+    {
+        title: "My First Blog",
+        content: "This is Content 1",
+        slug: "content-first"
+    },
+    {
+        title: "My Second Blog",
+        content: "This is Content 2",
+        slug: "content-second"
+    },
+    {
+        title: "My Third Blog",
+        content: "This is Content 3",
+        slug: "content-third"
+    },
+    {
+        title: "My Fourth Blog",
+        content: "This is Content 4",
+        slug: "content-fourth"
+    },
+    {
+        title: "My Firth Blog",
+        content: "This is Content 5",
+        slug: "content-firth"
+    },
+]
+
+module.exports = blogs;
--- Express/BlogExpress/routes/blog.js
+++ Express/BlogExpress/routes/blog.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const path = require('path');
+const blogs = require('../data/blogs');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../templates/index.html"));
+});
+
+router.get('/blog', (req, res) => {
+    blogs.forEach(e => {
+        console.log(e.title);
+    });
+    res.sendFile(path.join(__dirname, "../templates/blog.html"));
+});
+
+router.get('/blogpost/:slug', (req, res) => {
+    myBlogs = blogs.filter((e) => {
+        return e.slug == req.params.slug;
+    });
+    console.log(myBlogs);
+    res.sendFile(path.join(__dirname, "../templates/blogpage.html"));
 });
 
 module.exports = router;
