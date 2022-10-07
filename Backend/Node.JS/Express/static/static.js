9c958243e591ce508770a0754a0da54cd6d0b1ac
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-10-07 22:01:18 +0500

    Git Init

--- Backend/Node.JS/Express/static/static.js
+++ Backend/Node.JS/Express/static/static.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 4000);
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 app.get('/users', function (req, res) {
