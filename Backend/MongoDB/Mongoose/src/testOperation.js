242193f59ee0a4260acaf5408fa527394f7be707
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-10-14 22:46:10 +0500

    Git Init

---
+++ Backend/MongoDB/Mongoose/src/testOperation.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const addUser = require('./createUser');
+const User = require('./userModel');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+mongoose.connect("mongodb+srv://admin:admin1234@cluster0.jivsgsq.mongodb.net/testing?retryWrites=true&w=majority", { useNewUrlParser: true });
+
+mongoose.connection
+    .once('open', () => {
+        console.log('Yahoo...! Connection is Established');
+        addUser();
+    })
+    .on('error', (err) => {
+        console.log('Error: ', err);
+    })
+
+app.set('port', process.env.PORT || 4000);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/users', function (req, res) {
+    User.find({}) // All User's
+        .then((users) => {
+            console.log('Users ', users);
+            res.json(users);
+        })
+        .catch((err) => console.log('Error: ', err));
+});
+
+app.post('/user', (req, res) => {
+    console.log('req.body', req.body)
+    res.end(JSON.stringify(req.body));
+});
+
+app.listen(app.get('port'), function () {
+    console.log(`Express Started on: http://localhost:${app.get('port')}`);
+});
