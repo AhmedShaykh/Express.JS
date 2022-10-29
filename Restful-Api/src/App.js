b2891d25771a8d9cc8fdc6af4177ce576b9e44b0
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-10-29 20:35:33 +0500

    Git Init

--- Restful-Api/src/App.js
+++ Restful-Api/src/App.js
@@ -6,21 +6,19 @@ const app = express();
 
 const port = process.env.PORT || 4000;
 
-// app.use(express.json());
+app.use(express.json());
 
 app.post("/users", (req, res) => {
     console.log(req.body);
 
-    res.send("Hello");
-
-    // const data = new User(req.body);
-    // data.save().then(() => {
-    //     res.status(201).send(data);
-    //     return data;
-    // }).catch((e) => {
-    //     res.status(400).send(e);
-    //     console.log(e)
-    // });
+    const data = new User(req.body);
+    data.save().then(() => {
+        res.status(201).send(data);
+        return data;
+    }).catch((e) => {
+        res.status(400).send(e);
+        console.log(e)
+    });
 });
 
 app.listen(port, () => {
