4e138ec0af98d2b575072fd31e307dfdd136f65e
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-03 23:14:41 +0500

    Git Init

--- Authentication/Testing-Form/src/controllers/userController.js
+++ Authentication/Testing-Form/src/controllers/userController.js
@@ -1,12 +1,40 @@
 const userModel = require("../models/user");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const SECRET_KEY = "NOTESAPI";
 
 const signup = async (req, res) => {
 
     const { username, email, password } = req.body;
 
+    // ========== Existing User Check ========== //
+
     try {
 
-    } catch (error) {
+        const existingUser = await userModel.findOne({ email: email });
+
+        if (existingUser) {
+            return res.status(400).json({ message: "User Already Exists" });
+        }
+
+        // ========== Hashed Password ========== //
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // ========== Create User ========== //
+
+        const result = await userModel.create({
+            email: email,
+            password: hashedPassword,
+            username: username
+        });
+
+        // ========== Create Token ========== //
+
+        const token = jwt.sign({ email: result.email, id: result._id }, SECRET_KEY);
+    }
+
+    catch (error) {
 
     }
 };
