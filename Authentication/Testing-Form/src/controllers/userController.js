fa927951058fa1196c10d1437eb12e1cc3980f5c
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-03 20:32:17 +0500

    Git Init

---
+++ Authentication/Testing-Form/src/controllers/userController.js
@@ -0,0 +1,16 @@
+const userModel = require("../models/user");
+
+const signup = async (req, res) => {
+
+    const { username, email, password } = req.body;
+
+    try {
+
+    } catch (error) {
+
+    }
+};
+
+const signin = (req, res) => { };
+
+module.exports = { signup, signin };
