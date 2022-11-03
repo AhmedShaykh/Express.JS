12aac54e51ce8ef90a4a1ae135363d8f5e453e8a
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-04 00:05:33 +0500

    Git Init

--- Authentication/Testing-Form/src/controllers/userController.js
+++ Authentication/Testing-Form/src/controllers/userController.js
@@ -7,9 +7,9 @@ const signup = async (req, res) => {
 
     const { username, email, password } = req.body;
 
-    // ========== Existing User Check ========== //
-
     try {
+
+        // ========== Existing User Check ========== //
 
         const existingUser = await userModel.findOne({ email: email });
 
@@ -40,8 +40,43 @@ const signup = async (req, res) => {
         console.log(error);
         res.status(500).json({ message: "Somethin Went Worng!" });
     }
+
 };
 
-const signin = (req, res) => { };
+const signin = async (req, res) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        // ========== Existing User Check ========== //
+
+        const existingUser = await userModel.findOne({ email: email });
+
+        if (!existingUser) {
+            return res.status(404).json({ message: "User Not Found" });
+        }
+
+        // ========== Check Hashed Password ========== //
+
+        const matchPassword = await bcrypt.compare(password, existingUser.password);
+
+        if (!matchPassword) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+
+        // ========== Token Check ========== //
+
+        const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, SECRET_KEY);
+        res.status(201).json({ user: existingUser, token: token });
+
+    }
+
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Somethin Went Worng!" });
+    }
+
+};
 
 module.exports = { signup, signin };
