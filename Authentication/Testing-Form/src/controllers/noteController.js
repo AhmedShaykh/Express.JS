0a4a873c4b70622f36a3164648a597811985cf97
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-11-06 16:35:48 +0500

    Git Init

--- Authentication/Testing-Form/src/controllers/noteController.js
+++ Authentication/Testing-Form/src/controllers/noteController.js
@@ -25,12 +25,6 @@ const createNote = async (req, res) => {
 
 };
 
-const updateNote = async (req, res) => {
-};
-
-const deleteNote = async (req, res) => {
-};
-
 const getNote = async (req, res) => {
 
     try {
@@ -49,9 +43,55 @@ const getNote = async (req, res) => {
 
 };
 
+const updateNote = async (req, res) => {
+
+    const id = req.params.id;
+
+    const { title, description } = req.body;
+
+    const newNote = {
+        title: title,
+        description: description,
+        userId: req.userId
+    };
+
+    try {
+
+        await noteModel.findByIdAndUpdate(id, newNote, { new: true });
+        res.status(200).json(newNote);
+
+    }
+    catch (error) {
+
+        console.log(error);
+        res.status(500).json({ message: "Something Went Wrong" });
+
+    }
+
+};
+
+const deleteNote = async (req, res) => {
+
+    const id = req.params.id;
+
+    try {
+
+        const note = await noteModel.findByIdAndRemove(id);
+        res.status(202).json(note);
+
+    }
+    catch (error) {
+
+        console.log(error);
+        res.status(500).json({ message: "Something Went Wrong" });
+
+    }
+
+};
+
 module.exports = {
     createNote,
+    getNote,
     updateNote,
-    deleteNote,
-    getNote
+    deleteNote
 };
